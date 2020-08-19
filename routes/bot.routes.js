dafa7ce269b167427fef1d01a30c8da8558d68d3
masterit15 <masterit15@yandex.ru>
2020-08-19 08:24:22 +0300

    tg

--- routes/bot.routes.js
+++ routes/bot.routes.js
@@ -1,28 +1,33 @@
 const { Router } = require('express')
-const TG = require('telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1186953147:AAGDJAC-B5VbeMn3D5mk-Q3P1QlVuN0e9YA';
+const bot = new TelegramBot(token, { polling: true });
 const router = Router()
-const bot = new TG({ token: '1186953147:AAGDJAC-B5VbeMn3D5mk-Q3P1QlVuN0e9YA' })
 
-
-bot.getMe()
-  .then(res => {
-    //console.log(res)
-  })
-  .catch(console.err)
 /*
     метод получения
 */
 router.get('/', (req, res) => {
+
 
   bot.onText(/\/echo (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
     bot.sendMessage(chatId, resp);
   });
+
   bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Получили твое сообщение! Спасибо!');
   });
-  
+
+
 });
+
+
+
+
+
+
+
 module.exports = router
